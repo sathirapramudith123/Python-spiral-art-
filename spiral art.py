a76f9c7ab29ccd88beee5b718a0abda9c818baa6
Sathira Pramudith <sathirapramudith1@gmail.com>
2024-08-03 21:55:51 +0530

    spiral art

---
+++ spiral art.py
@@ -0,0 +1,6 @@
+#first we import the libary
+import turtle
+#define some colors
+colors = ['red','yellow','green','purple','blue','orange']
+#set up the turtle and set the background color
+t = turtle
